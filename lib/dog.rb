8bc59ecd3e386fc50f9c80a9aadb313fbde0bd6b
Sam kuriakose <samkuriakose26@gmail.com>
2019-03-28 19:29:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,10 @@
 class Dog
-  # code goes here
-end
+  attr_reader :name
+    attr_accessor :mood
+
+    def initialize (name, mood= "nervous")
+      @mood = mood
+      @name= name
+    # code goes here
+  end
+  end
